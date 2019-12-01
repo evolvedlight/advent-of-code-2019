c4e05e61049fcaf9e1fa758295351862b9c4f8f6
Stephen Brown <steve@evolvedlight.co.uk>
2019-12-01 18:18:59 +0100

    Day 1 solution

---
+++ AdventOfCode2019/Day1/FuelCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Day1
+{
+    public static class FuelCalculator
+    {
+        public static int Calculate(int mass)
+        {
+            return (int)Math.Floor(mass / 3m) - 2;
+        }
+
+        public static int CalculateIncludingItself(int mass)
+        {
+            var currentRequirement = Calculate(mass);
+            var total = 0;
+            while (currentRequirement > 0)
+            {
+                total += currentRequirement;
+                var fuelNeeded = Calculate(currentRequirement);
+                currentRequirement = fuelNeeded;
+            }
+            return total;
+        }
+
+        public static int CalculateIncludingItselfRecursive(int mass)
+        {
+            var fuelRequired = Calculate(mass);
+
+            if (fuelRequired <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return fuelRequired + CalculateIncludingItselfRecursive(fuelRequired);
+            }
+            
+        }
+    }
+}
