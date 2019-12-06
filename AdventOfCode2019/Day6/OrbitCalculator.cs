3f0995efbfe30d86c0b782db1d86db7a42c9b91b
Stephen Brown <steve@evolvedlight.co.uk>
2019-12-06 20:23:42 +0100

    Add day 6

---
+++ AdventOfCode2019/Day6/OrbitCalculator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Day6
+{
+    public static class OrbitCalculator
+    {
+        public static int CalculateNumberOfOrbits(List<string> input)
+        {
+            var orbits = input.ToDictionary(i => i.Split(")")[1], i => i.Split(")")[0]);
+            var orbitCount = 0;
+            foreach (var planet in orbits.Keys.Distinct())
+            {
+                var tempPlanet = planet;
+                while (orbits.ContainsKey(tempPlanet))
+                {
+                    orbitCount++;
+                    tempPlanet = orbits[tempPlanet];
+                }
+            }
+            return orbitCount;
+        }
+
+        public static int CalculatePathDistance(List<string> input)
+        {
+            var orbits = input.ToDictionary(i => i.Split(")")[1], i => i.Split(")")[0]);
+
+            var youPath = CalculatePathToBase(orbits, "YOU");
+            var sanPath = CalculatePathToBase(orbits, "SAN").ToHashSet();
+
+            var youDistance = 0;
+            var sanDistance = 0;
+            foreach (var planet in youPath)
+            {
+                if (sanPath.Contains(planet))
+                {
+                    var basePlanet = planet;
+
+                    var tempPlanet2 = "SAN";
+
+                    while (orbits[tempPlanet2] != basePlanet)
+                    {
+                        tempPlanet2 = orbits[tempPlanet2];
+                        sanDistance++;
+                    }
+                    return youDistance + sanDistance - 1;
+                }
+                youDistance++;
+            }
+            throw new Exception("Damn");   
+        }
+
+        public static List<string> CalculatePathToBase(Dictionary<string, string> orbits, string start)
+        {
+            var result = new List<string>();
+            var tempPlanet = start;
+            while (orbits.ContainsKey(tempPlanet))
+            {
+                result.Add(tempPlanet);
+                tempPlanet = orbits[tempPlanet];
+            }
+            return result;
+        }
+    }
+}
